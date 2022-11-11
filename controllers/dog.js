62361b627ea7dbcd0a95defe1d09e321f513bf29
Samyukthapandula <112123637+Samyukthapandula@users.noreply.github.com>
2022-11-10 23:51:11 -0600

    added resource for update

--- controllers/dog.js
+++ controllers/dog.js
@@ -51,9 +51,27 @@ exports.dog_delete = function(req, res) {
 }; 
  
 // Handle Dog update form on PUT. 
-exports.dog_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Dog update PUT' + req.params.id); 
-};
+exports.dog_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Dog.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Dog_age)  
+               toUpdate.Dog_age = req.body.Dog_age; 
+        if(req.body.Dog_breed) toUpdate.Dog_breed = req.body.Dog_breed; 
+        if(req.body.Dog_speciality) toUpdate.Dog_speciality = req.body.Dog_speciality; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+
+}; 
+
 
 // VIEWS 
 // Handle a show all view 
