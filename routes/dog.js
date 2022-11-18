3b9c96fe958f7aa4e808e0839a34f553aee0228c
Samyukthapandula <112123637+Samyukthapandula@users.noreply.github.com>
2022-11-17 20:43:06 -0600

    issue fix

--- routes/dog.js
+++ routes/dog.js
@@ -2,10 +2,7 @@ var express = require('express');
 const dog_controlers= require('../controllers/dog'); 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dog', { title: 'Search Results Dog' });
-});
+
 
 /* GET dogs */ 
 router.get('/', dog_controlers.dog_view_all_Page ); 
