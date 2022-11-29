74f92e2efd53e04159d08fd228c32488216e64b7
Samyukthapandula <112123637+Samyukthapandula@users.noreply.github.com>
2022-11-29 17:45:11 -0600

    added protection for update end point

--- routes/dog.js
+++ routes/dog.js
@@ -3,6 +3,17 @@ const dog_controlers= require('../controllers/dog');
 var router = express.Router();
 
 
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+
 
 /* GET dogs */ 
 router.get('/', dog_controlers.dog_view_all_Page ); 
@@ -15,7 +26,7 @@ router.get('/detail', dog_controlers.dog_view_one_Page);
 router.get('/create', dog_controlers.dog_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', dog_controlers.dog_update_Page);
+router.get('/update',secured,  dog_controlers.dog_update_Page);
 
 /* GET delete costume page */ 
 router.get('/delete', dog_controlers.dog_delete_Page); 
