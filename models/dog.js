c796d83c53cb3c276c53d8bdd97c988a69831a8b
Samyukthapandula <112123637+Samyukthapandula@users.noreply.github.com>
2022-11-29 19:37:11 -0600

    added bonus

--- models/dog.js
+++ models/dog.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose") 
 const dogSchema = mongoose.Schema({ 
-    Dog_age: Number, 
-    Dog_breed: String, 
+    Dog_age: {type:Number,min:1,max:6}, 
+    Dog_breed: {type:String,maxLength:20}, 
     Dog_speciality: String 
 }) 
 
--- routes/dog.js
+++ routes/dog.js
@@ -23,12 +23,12 @@ router.get('/detail', dog_controlers.dog_view_one_Page);
 
 
 /* GET create dog page */ 
-router.get('/create', dog_controlers.dog_create_Page); 
+router.get('/create',secured, dog_controlers.dog_create_Page); 
 
 /* GET create update page */ 
 router.get('/update',secured,  dog_controlers.dog_update_Page);
 
 /* GET delete costume page */ 
-router.get('/delete', dog_controlers.dog_delete_Page); 
+router.get('/delete', secured,dog_controlers.dog_delete_Page); 
 
 module.exports = router;
